a2ef3f9765c6c00c04b6e9173ee5a64556514cd2
RafaelMartins <rafaelsantosmartins.dev@gmail.com>
2025-03-07 11:19:03 +0000

    exercise 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("String: ");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Caráter: ");
+            char character = Console:ReadKey().KeyChar;
+            Console.WriteLine();
+
+            
+        }
+    }
+}
